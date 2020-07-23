5c84a2b3537d652fa5cf0887e93454e7a600aab4
Sibasish Mohanty <smsibasish@gmail.com>
2020-07-23 16:58:02 +0530

    favicons updated for the new logo

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,24 @@ class MyDocument extends Document {
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta content="#ffffff" name="theme-color" />
           <meta content="#ffffff" name="msapplication-TileColor" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
         </Head>
         <body>
           <Main />
