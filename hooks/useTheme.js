f10bf15864f864ff69737c2f270119de468503e1
Sibasish Mohanty <smsibasish@gmail.com>
2020-08-07 20:50:58 +0530

    fixes in custom hook

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,11 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useTheme = (defaultTheme, key) => {
-  const [theme, setTheme] = useState(window.localStorage.getItem(key) || defaultTheme);
-
-  useEffect(() => {
-    setTheme(localStorage.getItem(key));
-  }, [key]);
+  const [theme, setTheme] = useState(() => window.localStorage.getItem(key) || defaultTheme);
 
   useEffect(() => {
     window.localStorage.setItem(key, theme);
