13e7c50c4e18bed8a72bfa262afa5f70fd1808b5
Sibasish Mohanty <smsibasish@gmail.com>
2020-09-13 17:29:44 +0530

    fixed broken links, date formatting

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,21 +1,26 @@
+import tinytime from 'tinytime';
 import Link from 'next/link';
+
+const dateFormat = tinytime('{MMMM} {DD}, {YYYY}');
 
 const BlogCard = ({ link, date, title, children }) => {
   return (
     <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0">
       <dl>
         <dt className="sr-only">Published on</dt>
-        <dd className="text-base leading-6 font-medium text-text-offset">{date}</dd>
+        <dd className="text-base leading-6 font-medium text-text-offset">
+          {dateFormat.render(new Date(date))}
+        </dd>
       </dl>
       <div className="space-y-5 xl:col-span-3">
         <h2 className="text-2xl leading-8 font-bold tracking-tight">
-          <Link href={`/blog/[${link}]`} as="/blog/link">
+          <Link href="/blog/[link]" as={`/blog${link}`}>
             <a className="hover:text-primary">{title}</a>
           </Link>
         </h2>
         <div className="text-text-offset prose max-w-none">{children}</div>
         <div className="text-base leading-6 font-medium">
-          <Link href={link}>
+          <Link href="/blog/[link]" as={`/blog${link}`}>
             <a className="text-primary hover:underline" aria-label={`Read ${title}`}>
               Read more &rarr;
             </a>
